bf603e5806a008dab763acd80a8922f1fdfd2e5c
razvi <razvan.uleia@gmail.com>
2025-04-25 17:29:33 +0300

    fixed prometheus2

--- backend/src/main/java/org/example/xlr8travel/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/org/example/xlr8travel/security/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,6 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private JwtUtils jwtUtils;
 
     @Autowired
+    @Qualifier("userRepoUserDetailsService")
     private UserDetailsService userDetailsService;
 
     @Override
--- backend/src/main/java/org/example/xlr8travel/security/UserRepoUserDetailsService.java
+++ backend/src/main/java/org/example/xlr8travel/security/UserRepoUserDetailsService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Service("userRepoUserDetailsService")
 public class UserRepoUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
